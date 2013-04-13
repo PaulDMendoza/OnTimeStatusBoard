56521dcc822bed116c965603ab4ba2006d3da078
Paul Mendoza <paul@pauldmendoza.com>
2013-04-13 10:03:13 -0700

    GitHub for Windows is being a pain...

---
+++ StatusBoard/StatusBoard.Tests/TestConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusBoard.Tests
+{
+    public class TestConfig
+    {
+        public static string OnTimeClientID
+        {
+            get { return ConfigurationManager.AppSettings["OnTimeClientID"]; }
+        }
+
+        public static string OnTimeClientSecret
+        {
+            get { return Environment.GetEnvironmentVariable("OnTimeClientSecret", EnvironmentVariableTarget.Machine); }
+        }
+
+        public static string OnTimeTestingUserToken
+        {
+            get { return Environment.GetEnvironmentVariable("OnTimeTestingUserToken", EnvironmentVariableTarget.Machine); }
+        }
+
+        public static string OnTimeBaseUrl
+        {
+            get { return ConfigurationManager.AppSettings["OnTimeTestUserUrl"]; }
+            
+        }
+    }
+}
